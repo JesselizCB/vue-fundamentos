b43d0435f69148f2878dc92e6d80db351d3ae230
jesselizcb <choque101108@gmail.com>
2022-05-05 21:12:15 -0500

    indecision api

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <Counter title="Entraron" :start="102" />
-  <Counter />
+  <!-- <Counter title="Entraron" :start="102" /> -->
+  <!-- <Counter /> -->
+  <Indecision />
 </template>
 
 <script>
-import Counter from "./components/Counter.vue";
+// import Counter from "./components/Counter.vue";
+import Indecision from "./components/Indecision.vue";
 
 export default {
   name: "App",
   components: {
-    Counter,
+    // Counter,
+    Indecision,
   },
 };
 </script>
